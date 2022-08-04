bd80a09460d45398fa3d6bafe682d88eb36850a1
vitaliy-kro <krochakv6@gmail.com>
2022-08-04 13:37:57 +0300

    Completed task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ingredientsEl = document.querySelector('#ingredients');
+const elements = ingredients.map(value => {
+  const itemEl = document.createElement('li');
+  itemEl.textContent = value;
+  itemEl.classList.add('item');
+  return itemEl;
+});
+ingredientsEl.append(...elements);
